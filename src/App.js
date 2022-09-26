3e86a17872dc95d5c47c340d28ca62dc0464078f
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-09-26 21:39:34 +0530

    App interface Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1> Cash Register Manager App </h1>
+
+      <div className="AppContainer">
+        <p className="AppDesc">
+          Enter the bill amount given by Customer & know the minimum No.of Notes
+          to Return{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className="amountInputArea">
+          <label htmlFor="billAmount" className="Label">
+            Bill Amount :
+          </label>
+
+          <input
+            id="billAmount"
+            type="text"
+            placeholder="Enter Bill Amount"
+            className="inputBox"
+          />
+
+          <label htmlFor="cashGiven" className="Label">
+            Cash Given :
+          </label>
+
+          <input
+            id="cashGiven"
+            type="text"
+            placeholder="Enter Cash Given"
+            className="inputBox"
+          />
+
+          <button className="buttonCheck">Check</button>
+
+          <table className="returnTable">
+            <caption>Return Change</caption>
+            <tr>
+              <th>No.of Notes</th>
+              <td> </td>
+              <td> </td>
+              <td> </td>
+              <td> </td>
+              <td> </td>
+              <td> </td>
+              <td> </td>
+              <td> </td>
+            </tr>
+            <tr>
+              <th>Note</th>
+              <td>2000</td>
+              <td>500</td>
+              <td>100</td>
+              <td>50</td>
+              <td>20</td>
+              <td>10</td>
+              <td>5</td>
+              <td>1</td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
